ea3a9f71be4ad69e07492f31070aa5460e5c2ba6
alvin ony <aony75@gmail.com>
2020-02-22 13:29:21 -0600

    Updates

---
+++ precache-manifest.4e97055f7b4e0e6d642c8e9351ccbc2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af0ef86c8fb4ade7fc2fc4697eeb60d",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "df1bf51d973ed737dff4",
+    "url": "/react-practice/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "926a18919b3ed486db96",
+    "url": "/react-practice/static/js/2.ed22e44e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-practice/static/js/2.ed22e44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1bf51d973ed737dff4",
+    "url": "/react-practice/static/js/main.92614647.chunk.js"
+  },
+  {
+    "revision": "a00006a7019f273a6280",
+    "url": "/react-practice/static/js/runtime-main.1dfce90d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
